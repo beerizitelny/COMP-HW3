2e09edc22bc0cc4abd9589af24972daf2a111e4b
beerizitelny <beerizitelny@campus.technion.ac.il>
2025-05-31 16:46:38 +0300

    first SymTable class implemented

---
+++ SymbolTable.cpp
@@ -0,0 +1,54 @@
+//
+// Created by baeri on 31/05/2025.
+//
+
+#include "SymbolTable.hpp"
+
+SymTable::~SymTable() {
+    if (entries) {
+        // Delete each SymTableEntry* in the vector
+        for (auto entry : *entries) {
+            delete entry;
+        }
+        // Delete the vector itself
+        delete entries;
+    }
+}
+
+void SymTable::add_new_entry(SymTableEntry *entry) {
+    entries->push_back(entry);
+}
+
+SymTableStack::~SymTableStack() {
+    while (!sym_tables->empty()){
+        SymTable* sym_table = sym_tables->top();
+        delete sym_table;
+        sym_tables->pop();
+    }
+    delete sym_tables;
+    delete offsets;
+}
+
+void SymTableStack::push_table() {
+    sym_tables->push(new SymTable);
+    offsets->push(offsets->top());
+}
+
+void SymTableStack::pop_table() {
+    if (!sym_tables->empty() && !offsets->empty()){
+        delete sym_tables->top();
+        sym_tables->pop();
+        offsets->pop();
+    }
+}
+
+void SymTableStack::push_entry(SymTableEntry *entry) {
+    sym_tables->top()->add_new_entry(entry);
+    unsigned int offset = 0;
+    if (!offsets->empty()){
+        offset = offsets->top();
+        offsets->pop();
+    }
+    offset += entry->get_offset();
+    offsets->push(offset);
+}
---
+++ SymbolTable.hpp
@@ -0,0 +1,72 @@
+//
+// Created by baeri on 31/05/2025.
+//
+
+#ifndef COMP_HW3_SYMBOLTABLE_HPP
+#define COMP_HW3_SYMBOLTABLE_HPP
+
+#include "nodes.hpp"
+#include <vector>
+#include <stack>
+#include <string>
+
+/*
+ * This files takes care of the Symbol Tables uses in the HW.
+ * Each line (entry) of the ST represents a variable in the code
+ * and has it's name, type and offset.
+ * Note: each scope has it's own ST.
+ */
+
+class SymTableEntry {
+    std::string name;
+    ast::BuiltInType type;
+    unsigned int offset;
+
+public:
+    SymTableEntry(const std::string& name, ast::BuiltInType type, unsigned int offset) :
+        name(name), type(type), offset(offset) {};
+
+    unsigned int get_offset() const {return offset;}
+};
+
+class SymTable {
+    std::vector<SymTableEntry*>* entries;
+
+public:
+    SymTable() : entries(new std::vector<SymTableEntry*>()) {};
+    ~SymTable();
+    /* append a new entry in the table */
+    void add_new_entry(SymTableEntry* entry);
+};
+
+class SymTableStack {
+    std::stack<SymTable*>* sym_tables;
+    std::stack<unsigned int>* offsets;
+
+public:
+    SymTableStack() : sym_tables(new std::stack<SymTable*>), offsets(new std::stack<unsigned int>) {
+        sym_tables->push(new SymTable());
+        offsets->push(0);
+    };
+    ~SymTableStack();
+
+    /*
+     * pushes a new table to the stack.
+     * use in case of a new scope.
+     */
+    void push_table();
+
+    /*
+     * pops the top table from the stack.
+     * use at the end of scope.
+     */
+    void pop_table();
+
+    /*
+     * pushes a new entry to the top table.
+     * use in case of a new declaration.
+     */
+    void push_entry(SymTableEntry* entry);
+};
+
+#endif //COMP_HW3_SYMBOLTABLE_HPP
